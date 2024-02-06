6e468ec4a8a6aa99d2ee5f1df1fa90a898719328
Roberto Frenna <roberto.frenna@gmail.com>
2024-02-06 02:03:45 +0000

    ecodan: send metrics with timestamp & log invalid values
    Sending metrics with timestamps allows Prometheus to discard stale
    points (which can happen if the collector has been stuck for a while).
    This fixes the long-standing issue where metrics would get stale on
    any permanent error condition.

--- src/driver/ecodan/ecodan_metrics.go
+++ src/driver/ecodan/ecodan_metrics.go
@@ -1,122 +1,125 @@
 package ecodan
 
 import (
-	"fmt"
+  "time"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"rbf.dev/melcloud_prometheus_exporter/driver"
+  "github.com/prometheus/client_golang/prometheus"
+  "rbf.dev/melcloud_prometheus_exporter/driver"
 )
 
 var (
-	descOperationMode = prometheus.NewDesc(
-		"ecodan_operation_mode",
-		"Operation mode for the whole ECODan machine. " + driver.OperationModeHelpString(),
-		nil,
-		nil,
-	)
-	descOperationModeZone = prometheus.NewDesc(
-		"ecodan_zone_operation_mode",
-		"Operation mode for individual ECODan zones." + driver.OperationModeHelpString(),
-		[]string{"zone_number"},
-		nil,
-	)
-	descHeatFlowTemperatureSetpoint = prometheus.NewDesc(
-		"ecodan_heat_flow_temperature_setpoint_celsius",
-		"Heat flow temperature setpoint for individual ECODan zones.",
-		[]string{"zone_number"},
-		nil,
-	)
-	descTankWaterTemperatureSetpoint = prometheus.NewDesc(
-		"ecodan_tank_temperature_setpoint_celsius",
-		"Tank temperature setpoint for the ECODan.",
-		nil,
-		nil,
-	)
-	descTankWaterTemperature = prometheus.NewDesc(
-		"ecodan_tank_temperature_celsius",
-		"Tank temperature for the ECODan.",
-		nil,
-		nil,
-	)
-	descForcedHotWater = prometheus.NewDesc(
-		"ecodan_forced_hot_water_on",
-		"Whether forced hot water mode is ON for the ECODan.",
-		nil,
-		nil,
-	)
-	descOutdoorTemperature = prometheus.NewDesc(
-		"ecodan_outdoor_temperature_celsius",
-		"Outdoor temperature retrieved by the ECODan.",
-		nil,
-		nil,
-	)
-	descPower = prometheus.NewDesc(
-		"ecodan_power_on",
-		"Power state of the ECODan.",
-		nil,
-		nil,
-	)
-	descOffline = prometheus.NewDesc(
-		"ecodan_offline",
-		"Whether the ECODan is offline.",
-		nil,
-		nil,
-	)
-	allDescriptors = []*prometheus.Desc{
-		descOperationMode,
-		descOperationModeZone,
-		descHeatFlowTemperatureSetpoint,
-		descTankWaterTemperatureSetpoint,
-		descTankWaterTemperature,
-		descForcedHotWater,
-		descOutdoorTemperature,
-		descPower,
-		descOffline,
-	}
+  descOperationMode = prometheus.NewDesc(
+    "ecodan_operation_mode",
+    "Operation mode for the whole ECODan machine. " + driver.OperationModeHelpString(),
+    nil,
+    nil,
+  )
+  descOperationModeZone = prometheus.NewDesc(
+    "ecodan_zone_operation_mode",
+    "Operation mode for individual ECODan zones." + driver.OperationModeHelpString(),
+    []string{"zone_number"},
+    nil,
+  )
+  descHeatFlowTemperatureSetpoint = prometheus.NewDesc(
+    "ecodan_heat_flow_temperature_setpoint_celsius",
+    "Heat flow temperature setpoint for individual ECODan zones.",
+    []string{"zone_number"},
+    nil,
+  )
+  descTankWaterTemperatureSetpoint = prometheus.NewDesc(
+    "ecodan_tank_temperature_setpoint_celsius",
+    "Tank temperature setpoint for the ECODan.",
+    nil,
+    nil,
+  )
+  descTankWaterTemperature = prometheus.NewDesc(
+    "ecodan_tank_temperature_celsius",
+    "Tank temperature for the ECODan.",
+    nil,
+    nil,
+  )
+  descForcedHotWater = prometheus.NewDesc(
+    "ecodan_forced_hot_water_on",
+    "Whether forced hot water mode is ON for the ECODan.",
+    nil,
+    nil,
+  )
+  descOutdoorTemperature = prometheus.NewDesc(
+    "ecodan_outdoor_temperature_celsius",
+    "Outdoor temperature retrieved by the ECODan.",
+    nil,
+    nil,
+  )
+  descPower = prometheus.NewDesc(
+    "ecodan_power_on",
+    "Power state of the ECODan.",
+    nil,
+    nil,
+  )
+  descOffline = prometheus.NewDesc(
+    "ecodan_offline",
+    "Whether the ECODan is offline.",
+    nil,
+    nil,
+  )
+  allDescriptors = []*prometheus.Desc{
+    descOperationMode,
+    descOperationModeZone,
+    descHeatFlowTemperatureSetpoint,
+    descTankWaterTemperatureSetpoint,
+    descTankWaterTemperature,
+    descForcedHotWater,
+    descOutdoorTemperature,
+    descPower,
+    descOffline,
+  }
 
 )
 
 type StatsProvider interface {
-	Stats() *EcodanStatistics
+  Stats() (*EcodanStatistics, time.Time)
 }
 
 type collector struct {
-	provider StatsProvider
+  provider StatsProvider
 }
 
 func toBool(v bool) float64 {
-	if v {
-		return 1
-	}
-	return 0
+  if v {
+    return 1
+  }
+  return 0
+}
+
+func sendWithTimestamp(ch chan<- prometheus.Metric, t time.Time, m prometheus.Metric) {
+  ch <- prometheus.NewMetricWithTimestamp(t, m)
 }
 
 func (collector collector) Describe(ch chan<- *prometheus.Desc) {
-	prometheus.DescribeByCollect(collector, ch)
+  for _, desc := range allDescriptors {
+    ch <- desc
+  }
 }
 
 func (collector collector) Collect(ch chan<- prometheus.Metric) {
-	stats := collector.provider.Stats()
+  stats, t := collector.provider.Stats()
 
-	if stats == nil {
-		for _, desc := range allDescriptors {
-			ch <- prometheus.NewInvalidMetric(desc, fmt.Errorf("No statistics available yet for this device"))
-		}
-		return
-	}
+  if stats == nil {
+    return
+  }
 
-	ch <- prometheus.MustNewConstMetric(descOperationMode, prometheus.GaugeValue, float64(stats.OperationMode))
-	ch <- prometheus.MustNewConstMetric(descOperationModeZone, prometheus.GaugeValue, float64(stats.OperationModeZone1), "1")
-	ch <- prometheus.MustNewConstMetric(descHeatFlowTemperatureSetpoint, prometheus.GaugeValue, float64(stats.SetHeatFlowTemperatureZone1), "1")
-	ch <- prometheus.MustNewConstMetric(descTankWaterTemperatureSetpoint, prometheus.GaugeValue, float64(stats.SetTankWaterTemperature))
-	ch <- prometheus.MustNewConstMetric(descTankWaterTemperature, prometheus.GaugeValue, float64(stats.TankWaterTemperature))
-	ch <- prometheus.MustNewConstMetric(descForcedHotWater, prometheus.GaugeValue, toBool(stats.ForcedHotWaterMode))
-	ch <- prometheus.MustNewConstMetric(descOutdoorTemperature, prometheus.GaugeValue, float64(stats.OutdoorTemperature))
-	ch <- prometheus.MustNewConstMetric(descPower, prometheus.GaugeValue, toBool(stats.Power))
-	ch <- prometheus.MustNewConstMetric(descOffline, prometheus.GaugeValue, toBool(stats.Offline))
+  sendWithTimestamp(ch, t, prometheus.MustNewConstMetric(descOperationMode, prometheus.GaugeValue, float64(stats.OperationMode)))
+  sendWithTimestamp(ch, t, prometheus.MustNewConstMetric(descOperationModeZone, prometheus.GaugeValue, float64(stats.OperationModeZone1), "1"))
+  sendWithTimestamp(ch, t, prometheus.MustNewConstMetric(descHeatFlowTemperatureSetpoint, prometheus.GaugeValue, float64(stats.SetHeatFlowTemperatureZone1), "1"))
+  sendWithTimestamp(ch, t, prometheus.MustNewConstMetric(descTankWaterTemperatureSetpoint, prometheus.GaugeValue, float64(stats.SetTankWaterTemperature)))
+  sendWithTimestamp(ch, t, prometheus.MustNewConstMetric(descTankWaterTemperature, prometheus.GaugeValue, float64(stats.TankWaterTemperature)))
+  sendWithTimestamp(ch, t, prometheus.MustNewConstMetric(descForcedHotWater, prometheus.GaugeValue, toBool(stats.ForcedHotWaterMode)))
+  sendWithTimestamp(ch, t, prometheus.MustNewConstMetric(descOutdoorTemperature, prometheus.GaugeValue, float64(stats.OutdoorTemperature)))
+  sendWithTimestamp(ch, t, prometheus.MustNewConstMetric(descPower, prometheus.GaugeValue, toBool(stats.Power)))
+  sendWithTimestamp(ch, t, prometheus.MustNewConstMetric(descOffline, prometheus.GaugeValue, toBool(stats.Offline)))
 }
 
 func RegisterCollector(provider StatsProvider, reg prometheus.Registerer) {
-	collector := collector{provider}
-	reg.MustRegister(collector)
+  collector := collector{provider}
+  reg.MustRegister(collector)
 }
--- src/driver/ecodan/ecodan_stats_manager.go
+++ src/driver/ecodan/ecodan_stats_manager.go
@@ -2,7 +2,9 @@ package ecodan
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	"strings"
 	"sync"
 	"time"
 
@@ -11,44 +13,49 @@ import (
 )
 
 type statsManager struct {
-	mu sync.RWMutex
-	lastStats *EcodanStatistics
+    mu sync.RWMutex
+    lastStats *EcodanStatistics
+    lastUpdate time.Time
 }
 
 func NewDefaultStatsManager() driver.StatsManager {
-	return &statsManager{}
+    return &statsManager{}
 }
 
 func (s *statsManager) updateStats(stats *EcodanStatistics) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+    s.mu.Lock()
+    defer s.mu.Unlock()
 
-	s.lastStats = stats
+    s.lastStats = stats
+    s.lastUpdate = time.Now()
 }
 
 func (s *statsManager) ParseAndUpdateStats(reader io.ReadCloser) (*driver.Update, error) {
     var statistics EcodanStatistics
 
-    if err := json.NewDecoder(reader).Decode(&statistics); err != nil {
-    	return nil, err
+    var buf strings.Builder
+    tee := io.TeeReader(reader, &buf)
+
+    if err := json.NewDecoder(tee).Decode(&statistics); err != nil {
+        return nil, fmt.Errorf("while parsing '%.100v': %w", buf.String(), err)
     }
 
     s.updateStats(&statistics)
 
     return &driver.Update{
-    	NextCommunication: time.Time(statistics.NextCommunication),
+        NextCommunication: time.Time(statistics.NextCommunication),
     }, nil
 }
 
 func (s *statsManager) RegisterMetrics(reg prometheus.Registerer) {
-	RegisterCollector(s, reg)
+    RegisterCollector(s, reg)
 }
 
-func (s *statsManager) Stats() *EcodanStatistics {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
+func (s *statsManager) Stats() (*EcodanStatistics, time.Time) {
+    s.mu.RLock()
+    defer s.mu.RUnlock()
 
-	// Safe to unlock after returning the current value of the `lastStats` pointer as the
-	// stats object is never mutated, the pointer is just swapped around.
-	return s.lastStats
+    // Safe to unlock after returning the current value of the `lastStats` pointer as the
+    // stats object is never mutated, the pointer is just swapped around.
+    return s.lastStats, s.lastUpdate
 }
